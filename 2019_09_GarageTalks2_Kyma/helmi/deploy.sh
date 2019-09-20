16e67c2534ed8e8fad0a7558fe9c43d2aa3b079d
Michal Wcislo <mwcislo999@gmail.com>
2019-09-20 07:34:59 +0200

    Cloud Lego with Kyma Garage Talks 2

---
+++ 2019_09_GarageTalks2_Kyma/helmi/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# deploy helmi
+kubectl create -f $(dirname $(readlink --canonicalize-existing "$0"))/kube-helmi-rbac.yaml
+kubectl create -f $(dirname $(readlink --canonicalize-existing "$0"))/kube-helmi-secret.yaml
+kubectl create -f $(dirname $(readlink --canonicalize-existing "$0"))/kube-helmi.yaml
+kubectl apply -f $(dirname $(readlink --canonicalize-existing "$0"))/helmi.yml
